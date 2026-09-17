c9a41853f0955dbfdefde5d229b94f912c011af5
Mohit Kumar <mokumar7295@gmail.com>
2026-09-17 09:02:07 +0530

    Time: 4 ms (87.43%) | Memory: 44.6 MB (54.51%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,24 +1,44 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        
-        if (Math.abs(target) > sum || (sum + target) % 2 != 0) {
+       int n=nums.length;
+       int totalSum=0;
+       for(int i=0;i<n;i++){
+        totalSum+=nums[i];
+       }
+
+       int sum=(totalSum+target)/2;
+        if (Math.abs(target) > totalSum) {
             return 0;
         }
-        
-        int subsetSum = (sum + target) / 2;
-        int[] dp = new int[subsetSum + 1];
-        dp[0] = 1; 
-        
-        for (int num : nums) {
-            for (int j = subsetSum; j >= num; j--) {
-                dp[j] += dp[j - num];
-            }
+        if((totalSum+target) %2 !=0){
+           return 0;
         }
-        
-        return dp[subsetSum];
+
+       int[][] dp=new int[n+1][sum+1];
+
+       for(int i=0;i<=n;i++){
+        for(int j=0;j<=sum;j++){
+            dp[i][j]=-1;
+        }
+       }
+       return solve(nums,n,sum,dp);
+    }
+    private static int solve(int[] nums,int i,int sum,int[][] dp){
+        if(i==0){
+            if(sum==0){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+        int notTake=solve(nums,i-1,sum,dp);
+        int take=0;
+        if(nums[i-1]<=sum){
+            take=solve(nums,i-1,sum-nums[i-1],dp);
+        }
+        dp[i][sum]=take+notTake;
+        return dp[i][sum];
     }
 }
